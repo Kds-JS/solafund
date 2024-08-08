b45713d6de54ce2013a5c76ac7c6336d05dd8c70
Koudous Salifou <koudous@brumaire.io>
2024-08-08 16:14:09 +0200

    feat: add select network component

---
+++ src/components/admin-panel/select-network.tsx
@@ -0,0 +1,33 @@
+"use client";
+
+import React, { useContext } from 'react'
+import {
+    Select,
+    SelectContent,
+    SelectGroup,
+    SelectItem,
+    SelectLabel,
+    SelectTrigger,
+    SelectValue,
+  } from "@/components/ui/select"
+import { SessionContext } from '../wallets/sessions';
+import { NetworkName } from '@/types';
+
+export const SelectNetwork = () => {
+    const { setSelectedNetwork } = useContext(SessionContext);
+
+  return (
+    <Select defaultValue={NetworkName.Devnet} onValueChange={(value) => setSelectedNetwork(value)}>
+      <SelectTrigger className="w-[180px]">
+        <SelectValue placeholder="Select Network" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectGroup>
+          <SelectLabel>Select Network</SelectLabel>
+          <SelectItem value={NetworkName.Mainnet}>{NetworkName.Mainnet}</SelectItem>
+          <SelectItem value={NetworkName.Devnet}>{NetworkName.Devnet}</SelectItem>
+        </SelectGroup>
+      </SelectContent>
+    </Select>
+  )
+}
